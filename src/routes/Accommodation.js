a9fce0abd94c6df1a8221b5e27e025f29ccb9af4
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-25 09:51:20 +0200

    feat: implement app.use /accommodations in server.js

--- src/routes/Accommodation.js
+++ src/routes/Accommodation.js
@@ -1,6 +1,6 @@
 const Accommodation = required("./models/Accommodation");
 const router =required("express").Router();
 
-
+router.post()
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/users", UserRouter);
+app.use("/accommodations", require("./routes/Accommodation"));
 
 connectDB().then(() => {
   app.listen(port, () => {
